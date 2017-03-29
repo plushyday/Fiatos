e1bdafd6421d45d4585b9394b9fe697f546e2fb6
Anastasia <plushik.n@gmail.com>
2017-03-29 11:54:01 +0300

    icons+

--- js.js
+++ js.js
@@ -12,7 +12,7 @@ $(".et-search-field").on('blur', function () {
 //SEARCH ACTIVATION END
 
 // SLIDER START
-$('.slider').each(function() {
+$('.slider').each(function(){
   var $this = $(this);
   var $group = $this.find('.slide_group');
   var $slides = $this.find('.slide');
@@ -20,7 +20,7 @@ $('.slider').each(function() {
   var currentIndex = 0;
   var timeout;
   
-function move(newIndex) {
+  function move(newIndex) {
     var animateLeft, slideLeft;
     
     advance();
@@ -60,106 +60,87 @@ function move(newIndex) {
     });
   }
   
-function advance() {
+  function advance() {
    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      if (currentIndex < ($slides.length - 1)) {
-        move(currentIndex + 1);
-      } else {
-        move(0);
-      }
-    }, 5000);
-  }
-  
-  $('.next_btn').on('click', function() {
+   timeout = setTimeout(function() {
     if (currentIndex < ($slides.length - 1)) {
       move(currentIndex + 1);
     } else {
       move(0);
     }
-  });
-  
-  $('.previous_btn').on('click', function() {
-    if (currentIndex !== 0) {
-      move(currentIndex - 1);
-    } else {
-      move(3);
-    }
-  });
-  
-$.each($slides, function(index) {
-    var $button = $('<a class="slide_btn">&bull;</a>');
-    
-    if (index === currentIndex) {
-      $button.addClass('active');
-    }
-    $button.on('click', function() {
-      move(index);
-    }).appendTo('.slide_buttons');
-    bulletArray.push($button);
-  });
-  
-  advance();
+  }, 5000);
+ }
+
+ $('.next_btn').on('click', function() {
+  if (currentIndex < ($slides.length - 1)) {
+    move(currentIndex + 1);
+  } else {
+    move(0);
+  }
+});
+
+ $('.previous_btn').on('click', function() {
+  if (currentIndex !== 0) {
+    move(currentIndex - 1);
+  } else {
+    move(3);
+  }
+});
+
+ $.each($slides, function(index) {
+  var $button = $('<a class="slide_btn">&bull;</a>');
+
+  if (index === currentIndex) {
+    $button.addClass('active');
+  }
+  $button.on('click', function() {
+    move(index);
+  }).appendTo('.slide_buttons');
+  bulletArray.push($button);
+});
+
+ advance();
 });
 
 //SLIDER END
 
 //RANDOM COLOR
-var obj={
-	"0":"rgb(221,9,8)",
-	"1":"rgb(255,158,41)",
-	"2":"rgb(63,183,210)",
-	"3":"rgb(21,192,28)",
-	"4":"rgb(126,59,7)",
-	"5":"rgb(25,99,206)",
-	"6":"rgb(193,25,206)",
-	"7":"rgb(127,186,216)",
-	"8":"rgb(121,117,150)",
-	"9":"rgb(255,204,255)",
-	"10":"rgb(7,12,31)",
-	"11":"rgb(0,82,94)",
-	"12":"rgb(84,35,68)",
-	"13":"rgb(113,180,141)",
-	"14":"rgb(94,84,142)",
-	"15":"rgb(56,97,80)",
-	"16":"rgb(92,109,112)",
-	"17":"rgb(112,101,99)"
-}
+
+var obj=[
+"rgb(221,9,8)",
+"rgb(255,158,41)",
+"rgb(63,183,210)",
+"rgb(21,192,28)",
+"rgb(126,59,7)",
+"rgb(25,99,206)",
+"rgb(193,25,206)",
+"rgb(127,186,216)",
+"rgb(121,117,150)",
+"rgb(255,204,255)",
+"rgb(7,12,31)",
+"rgb(0,82,94)",
+"rgb(84,35,68)",
+"rgb(113,180,141)",
+"rgb(94,84,142)"];
 
 function randomInteger(min, max) {
-    var rand = min - 0.5 + Math.random() * (max - min + 1)
-    rand = Math.round(rand);
-    return rand;
+  var rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
 }
 
-var svgcolor = $(".svg-background")
+var svgcolor = $(".svg-background");
 
-//замыкания
-
-function createColor(){
-
-}
-
-svgcolor.each(function callbackcolor(index, element){
-
-	var rand=randomInteger(0,17);
+console.dir(svgcolor);
+svgcolor.each(function(){
+	var rand=randomInteger(0, obj.length-1);
 	var randcolor = obj[rand];
-again: 
-for (var i=0; i<svgcolor.length; i++)
-		{
-		if(svgcolor.eq(i).css("fill")==randcolor)
-				{
-			rand=randomInteger(0,17);
-			randcolor = obj[rand];
-			break again;
-			}
-		};
-	$(this).css("fill",randcolor);
-	
+  console.log(rand+" "+randcolor);
+  $(this).css({"fill":randcolor});
+  obj.splice(rand, 1);
+});
+//RANDOM COLOR END 
 });
 
-// $.each(svgcolor, function())
-// $(".svg-background").css("fill", randcolor);
-//RANDOM COLOR END
-})
+
 
